e413292bacfdc6c969823970d8a9eb58530a34c3
STS-TRUNGTRAN\trung.tran <trung.tran@saigontechnology.com>
2021-02-03 09:51:23 +0700

    Test Data binding finished

---
+++ WPF/WPFApp/DataBindingWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for DataBindingWindow.xaml
+    /// </summary>
+    public partial class DataBindingWindow : Window
+    {
+        // Create Person data source
+        Person person = new Person();
+
+        public DataBindingWindow()
+        {
+            InitializeComponent();
+
+            // Make data source available for binding
+            this.DataContext = person;
+
+            this.Closing += DataBindingWindow_Closing;
+        }
+
+        private void DataBindingWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show($"Final person name: {person.Name}");
+        }
+    }
+}
