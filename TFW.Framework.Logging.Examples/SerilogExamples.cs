cff8a32dd2c7f1878ee440b257083df880e32339
trung-tran-sts <trung.tran@saigontechnology.com>
2021-03-14 12:11:27 +0700

    example logging

--- TFW.Framework.Logging.Examples/SerilogExamples.cs
+++ TFW.Framework.Logging.Examples/SerilogExamples.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using Serilog;
+using Serilog.Context;
 using Serilog.Core;
+using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Filters;
+using Serilog.Sinks.Async;
 
 namespace TFW.Framework.Logging.Examples
 {
@@ -14,6 +18,15 @@ namespace TFW.Framework.Logging.Examples
         {
             logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("Username", Guid.NewGuid().ToString()));
         }
+    }
+
+    class MonitorConfiguration : IAsyncLogEventSinkMonitor
+    {
+        public void StartMonitoring(IAsyncLogEventSinkInspector inspector) =>
+            inspector.ToString();
+
+        public void StopMonitoring(IAsyncLogEventSinkInspector inspector)
+        { /* reverse of StartMonitoring */ }
     }
 
     public class DestructorPolicy : IDestructuringPolicy
@@ -30,15 +43,26 @@ namespace TFW.Framework.Logging.Examples
     {
         public static void Run()
         {
-            var template = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {LoggerName} {Message:lj}{Username:j}{NewLine}{Exception}";
+            var levelSwitch = new LoggingLevelSwitch(initialMinimumLevel: LogEventLevel.Debug);
+
+            SelfLog.Enable(Console.Out);
+            SelfLog.WriteLine("This is a self log");
+
+            var template = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {LoggerName} {ClientIp} {Message:lj}{Username:j}{NewLine}{Exception}";
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Error)
+                .Enrich.WithThreadId()
+                .Enrich.WithClientIp()
+                .Enrich.FromLogContext()
                 .Enrich.With<TestEnricher>()
                 .Destructure.ByTransforming<TestEnricher>(o => "He is enricher")
 #if false
                 .Destructure.With<DestructorPolicy>()
 #endif
-                .WriteTo.Console()
+                .WriteTo.Console(levelSwitch: levelSwitch, outputTemplate: template, formatProvider: CultureInfo.CurrentCulture)
+                .WriteTo.Async(cfg => cfg.File("logs/async.txt"), bufferSize: 1000, blockWhenFull: true, monitor: new MonitorConfiguration())
                 .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Hour, outputTemplate: template)
 #if false
                 .WriteTo.Logger(cfg =>
@@ -48,8 +72,24 @@ namespace TFW.Framework.Logging.Examples
 #endif
                 .CreateLogger();
 
+            using (LogContext.PushProperty("A", 1))
+            {
+                Log.Information("Carries property A = 1");
+
+                using (LogContext.PushProperty("A", 2))
+                using (LogContext.PushProperty("B", 1))
+                using (LogContext.PushProperty("ClientIp", "11.1..1.1.1.11"))
+                {
+                    Log.Information("Carries A = 2 and B = 1 {A}"); // wrong use of property
+                }
+
+                Log.Information("Carries property A = 1, again");
+            }
+
             var test = new TestEnricher();
-            Log.Information("Connected to {Enricher}", test);
+            var enricherContext = Log.ForContext<TestEnricher>()
+                .ForContext("ContextVal", "OKEOKE");
+            enricherContext.Information("Connected to {Enricher} {SourceContext} {ContextVal}", test); // wrong use of property
 
             var count = 456;
             Log.Information("Retrieved {Count} records", count);
