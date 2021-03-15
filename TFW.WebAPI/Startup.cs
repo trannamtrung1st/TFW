7af57af90e6ad1b45356142a6f23fc86ced0c0b2
trung-tran-sts <trung.tran@saigontechnology.com>
2021-03-15 22:58:23 +0700

    temp

--- TFW.WebAPI/Startup.cs
+++ TFW.WebAPI/Startup.cs
@@ -15,8 +15,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Serilog;
-using Serilog.Events;
 using TFW.Cross;
 using TFW.Cross.Entities;
 using TFW.Cross.Models.Setting;
