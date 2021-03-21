043b96ff189e12fd1258f94ee616d36683773ab3
trung-tran-sts <trung.tran@saigontechnology.com>
2021-03-21 15:35:59 +0700

    add file watcher

---
+++ TFW.Framework.Common.Examples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using TFW.Framework.Common.Helpers;
+
+namespace TFW.Framework.Common.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var watcher = new FileSystemWatcher(
+                @"T:\Workspace\Personal\TFW\TFW.WebAPI", "appsettings.json")
+                .IncludeSubdirs(false)
+                .NotifyFilter(NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size);
+
+            watcher.Changed += Watcher_Changed;
+
+            watcher.Start();
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("appsettings.json changed");
+        }
+    }
+}
---
+++ TFW.Framework.Common/Helpers/FileSystemWatcherHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TFW.Framework.Common.Helpers
+{
+    public static class FileSystemWatcherHelper
+    {
+        public static FileSystemWatcher Start(this FileSystemWatcher watcher)
+        {
+            watcher.EnableRaisingEvents = true;
+            return watcher;
+        }
+
+        public static FileSystemWatcher Stop(this FileSystemWatcher watcher)
+        {
+            watcher.EnableRaisingEvents = false;
+            return watcher;
+        }
+
+        public static FileSystemWatcher NotifyFilter(this FileSystemWatcher watcher, NotifyFilters notifyFilters)
+        {
+            watcher.NotifyFilter = notifyFilters;
+            return watcher;
+        }
+
+        public static FileSystemWatcher WatchOnly(this FileSystemWatcher watcher, string dir, string filter)
+        {
+            watcher.Path = dir;
+            return watcher.Filter(filter);
+        }
+
+        public static FileSystemWatcher Watch(this FileSystemWatcher watcher, string dir, params string[] filters)
+        {
+            watcher.Path = dir;
+            return watcher.AddFilters(filters);
+        }
+
+        public static FileSystemWatcher IncludeSubdirs(this FileSystemWatcher watcher, bool val)
+        {
+            watcher.IncludeSubdirectories = val;
+            return watcher;
+        }
+
+        public static FileSystemWatcher Filter(this FileSystemWatcher watcher, string filter)
+        {
+            watcher.Filter = filter;
+            return watcher;
+        }
+
+        public static FileSystemWatcher AddFilters(this FileSystemWatcher watcher, params string[] filters)
+        {
+            foreach (var filter in filters)
+                watcher.Filters.Add(filter);
+            return watcher;
+        }
+
+        public static FileSystemWatcher RemoveFilters(this FileSystemWatcher watcher, params string[] filters)
+        {
+            foreach (var filter in filters)
+                watcher.Filters.Remove(filter);
+            return watcher;
+        }
+
+        public static FileSystemWatcher ClearFilters(this FileSystemWatcher watcher, params string[] filters)
+        {
+            watcher.Filters.Clear();
+            return watcher;
+        }
+    }
+}
