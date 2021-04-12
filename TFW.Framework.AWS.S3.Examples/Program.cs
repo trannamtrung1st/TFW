6a7a1cc4dd72fd0909f07ac63f198c7075fccf89
STS-TRUNGTRAN\trung.tran <trung.tran@saigontechnology.com>
2021-04-12 09:39:09 +0700

    add S3 project

---
+++ TFW.Framework.AWS.S3.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TFW.Framework.AWS.S3.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
