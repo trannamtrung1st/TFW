166b4915446d22f7b964e8da48e0f68ab4dba8ee
trannamtrung1st <trannamtrung1st@gmail.com>
2021-05-23 01:26:19 +0700

    add join query for comparision

--- TFW.Framework.CQRSExamples/Queries/ReportQuery.cs
+++ TFW.Framework.CQRSExamples/Queries/ReportQuery.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TFW.Framework.CQRSExamples.Entities.Query;
 using TFW.Framework.CQRSExamples.Entities.Relational;
 using TFW.Framework.CQRSExamples.Models.Query;
+using TFW.Framework.i18n.Extensions;
 
 namespace TFW.Framework.CQRSExamples.Queries
 {
@@ -25,8 +26,9 @@ namespace TFW.Framework.CQRSExamples.Queries
             int fromMonth, int fromYear, int toMonth, int toYear)
         {
             DateTime from = new DateTime(fromYear, fromMonth, 1, 0, 0, 0, DateTimeKind.Utc);
-            DateTime to = new DateTime(toYear, toMonth, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime to = from.GetMonthEnd();
 
+#if true
             var reportList = await _queryDbContext.CustomerReports
                 .Where(o => o.MonthTime >= from && o.MonthTime <= to)
                 .Select(o => new CustomerReportListItem
@@ -46,6 +48,43 @@ namespace TFW.Framework.CQRSExamples.Queries
 
             foreach (var report in reportList)
                 report.CustomerName = customerNameMap.GetValueOrDefault(report.CustomerId, "[Deleted]");
+#else
+            var totalOrderQuery = _relationalContext.Orders
+                .Where(o => o.Time >= from && o.Time <= to)
+                .GroupBy(o => new { o.CustomerId, CustomerName = o.Customer.Name, o.Time.Month, o.Time.Year }).Select(o => new
+                {
+                    CustomerId = o.Key.CustomerId,
+                    CustomerName = o.Key.CustomerName,
+                    Month = o.Key.Month,
+                    Year = o.Key.Year,
+                    TotalOrder = o.Count()
+                });
+
+            var totalRevenueQuery = _relationalContext.OrderItems
+                .Where(o => o.Order.Time >= from && o.Order.Time <= to)
+                .GroupBy(o => new { o.Order.CustomerId, o.Order.Time.Month, o.Order.Time.Year }).Select(o => new
+                {
+                    CustomerId = o.Key.CustomerId,
+                    Month = o.Key.Month,
+                    Year = o.Key.Year,
+                    TotalRevenue = o.Sum(item => item.Quantity * item.UnitPrice)
+                });
+
+            var reportList = await (from totalOrder in totalOrderQuery
+                                    join totalRevenue in totalRevenueQuery
+                                    on new { totalOrder.CustomerId, totalOrder.Month, totalOrder.Year }
+                                    equals new { totalRevenue.CustomerId, totalRevenue.Month, totalRevenue.Year }
+                                    select new CustomerReportListItem
+                                    {
+                                        CustomerId = totalOrder.CustomerId,
+                                        CustomerName = totalOrder.CustomerName,
+                                        Id = "",
+                                        LastUpdatedTime = DateTime.UtcNow,
+                                        MonthTime = new DateTime(totalOrder.Year, totalOrder.Month, 1),
+                                        TotalOrder = totalOrder.TotalOrder,
+                                        TotalRevenue = totalRevenue.TotalRevenue
+                                    }).ToArrayAsync();
+#endif
 
             return reportList;
         }
