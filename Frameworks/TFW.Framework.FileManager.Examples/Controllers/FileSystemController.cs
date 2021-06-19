35ee54538629a135734bcf88fb18012c5721af38
trung-tran-sts <trung.tran@saigontechnology.com>
2021-06-19 22:12:09 +0700

    update elfinder

--- Frameworks/TFW.Framework.FileManager.Examples/Controllers/FileSystemController.cs
+++ Frameworks/TFW.Framework.FileManager.Examples/Controllers/FileSystemController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using elFinder.Net.AspNetCore.Extensions;
 using elFinder.Net.AspNetCore.Helper;
@@ -36,7 +37,7 @@ namespace TFW.Framework.FileManager.Examples.Controllers
         public async Task<IActionResult> Thumb(string target)
         {
             SetupConnector();
-            var thumb = await _connector.GetThumbAsync(target);
+            var thumb = await _connector.GetThumbAsync(target, HttpContext.RequestAborted);
             var actionResult = ConnectorHelper.GetThumbResult(thumb);
             return actionResult;
         }
@@ -45,17 +46,82 @@ namespace TFW.Framework.FileManager.Examples.Controllers
         {
             foreach (var volume in _volumes)
             {
-                volume.ItemAttributes = new HashSet<SpecificItemAttribute>()
+                var limitedFolder = $"{volume.RootDirectory}{volume.DirectorySeparatorChar}limited";
+                var haloFile = $"{volume.RootDirectory}{volume.DirectorySeparatorChar}halo.txt";
+
+                // The last matched rule will override all previous (if its flags are defined)
+                volume.ObjectAttributes = new List<FilteredObjectAttribute>()
                 {
-                    new SpecificItemAttribute($"{volume.RootDirectory}{volume.DirectorySeparatorChar}init")
+                    // You can implement your own logic to modify Physical File attributes to maintain the Access Control attributes 
+                    new FilteredObjectAttribute()
                     {
-                        Locked = true, Read = true, Write = false
+                        Expression = $"file.attributes.readonly", // Example only
+                        ObjectFilter = (obj) => (int)obj.Attributes != -1 && obj.Attributes.HasFlag(FileAttributes.ReadOnly),
+                        Write = false
                     },
-                    new SpecificItemAttribute($"{volume.RootDirectory}{volume.DirectorySeparatorChar}halo.txt")
+                    new FilteredObjectAttribute()
                     {
-                        Locked = true, Read = true, Write = false
-                    }
+                        Expression = $"file.attributes.hidden", // Example only
+                        ObjectFilter = (obj) => (int)obj.Attributes != -1 && obj.Attributes.HasFlag(FileAttributes.Hidden),
+                        Visible = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"obj.fullname = '{limitedFolder}'", // Example only
+                        ObjectFilter = (obj) => obj.FullName == limitedFolder,
+                        Locked = true, Read = false, Write = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"obj.fullname = '{haloFile}'", // Example only
+                        ObjectFilter = (obj) => obj.FullName == haloFile,
+                        Locked = true, Write = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"file.name.startsWith = 'secrets_'", // Example only
+                        FileFilter = (file) => file.Name.StartsWith("secrets_"),
+                        Locked = true, Write = false, Visible = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"file.mime = 'somemime-type'", // Example only
+                        FileFilter = (file) => file.MimeType == "somemime-type",
+                        Locked = true, Write = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"file.ext = 'exe'", // Example only
+                        FileFilter = (file) => file.Extension == ".exe",
+                        Locked = true, Write = false, ShowOnly = true, Read = false
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"dir.parent.name = 'locked'", // Example only
+                        DirectoryFilter = (dir) => dir.Parent.Name == "locked",
+                        Locked = true
+                    },
+                    new FilteredObjectAttribute()
+                    {
+                        Expression = $"dir.name = 'access-denied'", // Example only
+                        DirectoryFilter = (dir) => dir.Name == "access-denied",
+                        Access = false
+                    },
+                    //new FilteredObjectAttribute()
+                    //{
+                    //    Expression = $"obj.isroot", // Example only
+                    //    ObjectFilter = (obj) => volume.IsRoot(obj),
+                    //    Access = true, Visible = true, Read = true, Write = true, Locked = false, ShowOnly = false
+                    //},
                 };
+
+                // Or if you want to restrict all by default
+                //volume.DefaultObjectAttribute = new ObjectAttribute
+                //{
+                //    Visible = false,
+                //    Access = false
+                //};
+
                 _connector.AddVolume(volume);
                 volume.Driver.AddVolume(volume);
             }
