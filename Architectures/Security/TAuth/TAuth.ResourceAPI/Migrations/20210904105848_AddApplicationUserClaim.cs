e5ac59aca9c0fcde67bd6e17840d14444f9519c3
trannamtrung1st <trannamtrung1st@gmail.com>
2021-09-04 17:59:11 +0700

    Temp for tauth

---
+++ Architectures/Security/TAuth/TAuth.ResourceAPI/Migrations/20210904105848_AddApplicationUserClaim.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TAuth.ResourceAPI.Migrations
+{
+    public partial class AddApplicationUserClaim : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClaims", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserClaims");
+        }
+    }
+}
